cd7960bce7de031a2c7f9b68edd402a50e228511
774431978@qq.com <774431978@qq.com>
2020-12-15 21:20:19 +0800

    修改商品模块内部接口实现

--- goodsservice/src/main/java/cn/edu/xmu/goods/service/Impl/IGoodsServiceImpl.java
+++ goodsservice/src/main/java/cn/edu/xmu/goods/service/Impl/IGoodsServiceImpl.java
@@ -1,18 +1,18 @@
 package cn.edu.xmu.goods.service.Impl;
 
 
-import cn.edu.xmu.goods.dao.GoodsSkuDao;
-import cn.edu.xmu.goods.dao.GoodsSpuDao;
-import cn.edu.xmu.goods.model.bo.GoodsSku;
-import cn.edu.xmu.goods.model.po.GoodsSkuPo;
-import cn.edu.xmu.goods.model.po.GoodsSpuPo;
-import cn.edu.xmu.goods.model.vo.GoodsSkuRetVo;
-import cn.edu.xmu.goods.model.vo.GoodsSpuRetVo;
-import cn.edu.xmu.goods.service.GoodsSkuService;
-import cn.edu.xmu.goods.service.GoodsSpuService;
-import cn.edu.xmu.ooad.util.ReturnObject;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.transaction.annotation.Transactional;
+        import cn.edu.xmu.goods.dao.GoodsSkuDao;
+        import cn.edu.xmu.goods.dao.GoodsSpuDao;
+        import cn.edu.xmu.goods.model.bo.GoodsSku;
+        import cn.edu.xmu.goods.model.po.GoodsSkuPo;
+        import cn.edu.xmu.goods.model.po.GoodsSpuPo;
+        import cn.edu.xmu.goods.model.vo.GoodsSkuRetVo;
+        import cn.edu.xmu.goods.model.vo.GoodsSpuRetVo;
+        import cn.edu.xmu.goods.service.GoodsSkuService;
+        import cn.edu.xmu.goods.service.GoodsSpuService;
+        import cn.edu.xmu.ooad.util.ReturnObject;
+        import org.springframework.beans.factory.annotation.Autowired;
+        import org.springframework.transaction.annotation.Transactional;
 
 public class IGoodsServiceImpl {
 
@@ -22,16 +22,13 @@ public class IGoodsServiceImpl {
     @Autowired
     GoodsSpuDao goodsSpuDao;
     @Transactional
-    public GoodsSkuPo getSkuById(Long id){
+    public GoodsSku getSkuById(Long id){
         ReturnObject<GoodsSkuPo> goodsSkuReturnObject = goodsSkuDao.getSkuById(id);
         GoodsSkuPo goodsSkuPo = goodsSkuReturnObject.getData();
-        GoodsSku goodsSku = new GoodsSku(goodsSkuPo);
-
-        if(goodsSku != null){
+        if(goodsSkuPo == null){
             return null;
         }
-        GoodsSkuRetVo retVo = goodsSku.createVo();
-        GoodsSpuPo goodsSpuPo = goodsSpuDao.getGoodsSpuPoBySkuId(id).getData();
-        return goodsSkuPo;
+        GoodsSku goodsSku = new GoodsSku(goodsSkuPo);
+        return goodsSku;
     }
 }
